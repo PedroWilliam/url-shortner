2891c4169107533c7c177bc23eb8c54c56e379c6
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-12-04 23:52:41 -0300

    feat(TokenProvider): Add token increment functionality

--- Api/src/UrlShortener.Core/TokenProvider.cs
+++ Api/src/UrlShortener.Core/TokenProvider.cs
@@ -2,20 +2,22 @@
 
 public class TokenProvider
 {
+    private long _token = 0;
     private TokenRange? _tokenRange;
 
     public void AssignRange(long start, long end)
     {
-        _tokenRange = new TokenRange(start, end);
+        AssignRange(new TokenRange(start, end));
     }
 
     public void AssignRange(TokenRange tokenRange)
     {
         _tokenRange = tokenRange;
+        _token = _tokenRange.Start;
     }
 
     public long GetToken()
     {
-        return _tokenRange.Start;
+        return _token++;
     }
 }
---
+++ Api/tests/UrlShortener.Api.Core.Tests/TokenProviderScenarios.cs
@@ -0,0 +1,27 @@
+﻿using UrlShortener.Core;
+
+namespace UrlShortener.Api.Core.Tests;
+
+public class TokenProviderScenarios
+{
+    [Fact]
+    public void Should_get_the_token_from_start()
+    {
+        var provider = new TokenProvider();
+        
+        provider.AssignRange(5, 10);
+
+        provider.GetToken().Should().Be(5);
+    }
+
+    [Fact]
+    public void Should_increment_token_on_get()
+    {
+        var provider = new TokenProvider();
+
+        provider.AssignRange(5, 10);
+        provider.GetToken();
+
+        provider.GetToken().Should().Be(6);
+    }
+}
